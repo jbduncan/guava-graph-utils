b17976f1c878d23dfb491777d4b8d005692bbd1b
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2021-04-20 18:45:33 +0100

    Move tests for MoreGraphs.asValueGraph into their own class

--- src/test/java/org/jbduncan/MoreGraphsAsValueGraphTests.java
+++ src/test/java/org/jbduncan/MoreGraphsAsValueGraphTests.java
@@ -4,22 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableTable;
 import com.google.common.graph.ElementOrder;
 import com.google.common.graph.EndpointPair;
-import com.google.common.graph.GraphBuilder;
-import com.google.common.graph.ImmutableGraph;
-import java.util.Set;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.Test;
 
+
 @SuppressWarnings({
-    // We test methods that purposefully use a unstable Guava API
+    // We test a method that purposefully uses an unstable Guava API.
     "UnstableApiUsage",
-    // We also test that the methods react gracefully in the face of nulls.
+    // We also test that the method reacts gracefully in the face of nulls.
     "ConstantConditions"})
-class MoreGraphsTests {
+class MoreGraphsAsValueGraphTests {
 
   private static final String A_ROW_KEY = "aRowKey";
   private static final String A_COLUMN_KEY = "aColumnKey";
@@ -43,144 +40,6 @@ class MoreGraphsTests {
 
   private static <R, C, V> HashBasedTable<R, C, V> emptyMutableTable() {
     return HashBasedTable.create(emptyTable());
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndEmptyStartingNodes_thenResultIsEmptyGraph() {
-    // when
-    var result =
-        MoreGraphs.buildGraphWithBreadthFirstTraversal(
-            ImmutableList.of(), node -> ImmutableList.of("any old node"));
-
-    // then
-    assertThat(result).isEqualTo(GraphBuilder.directed().allowsSelfLoops(true).immutable().build());
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndEmptySuccessorsFunction_thenResultIsEqualToStartingNodes() {
-    // when
-    var result =
-        MoreGraphs.buildGraphWithBreadthFirstTraversal(
-            ImmutableList.of("any old node"), node -> ImmutableList.of());
-
-    // then
-    assertThat(result)
-        .isEqualTo(
-            GraphBuilder.directed()
-                .allowsSelfLoops(true)
-                .immutable()
-                .addNode("any old node")
-                .build());
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndCyclicSuccessorsFunction_thenResultTerminatesAndContainsCycle() {
-    // when
-    var result =
-        MoreGraphs.buildGraphWithBreadthFirstTraversal(
-            ImmutableList.of(1),
-            node -> (node * 2) <= 4 ? ImmutableList.of(node * 2) : ImmutableList.of(1));
-
-    // then
-    assertThat(result)
-        .isEqualTo(
-            GraphBuilder.directed()
-                .allowsSelfLoops(true)
-                .immutable()
-                .putEdge(1, 2)
-                .putEdge(2, 4)
-                .putEdge(4, 1)
-                .build());
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndTreeShapedSuccessorsFunction_thenResultContainsTree() {
-    // when
-    var result =
-        MoreGraphs.buildGraphWithBreadthFirstTraversal(
-            ImmutableList.of(1),
-            node -> {
-              if (node == 1) {
-                return ImmutableList.of(2, 3);
-              }
-              return ImmutableList.of();
-            });
-
-    // then
-    assertThat(result)
-        .isEqualTo(
-            GraphBuilder.directed()
-                .allowsSelfLoops(true)
-                .immutable()
-                .putEdge(1, 2)
-                .putEdge(1, 3)
-                .build());
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndDirectedSelfLoopingGuavaGraph_thenResultIsIdentical() {
-    // given
-    var expectedGraph =
-        GraphBuilder.directed().allowsSelfLoops(true).<Integer>immutable().putEdge(1, 1).build();
-
-    // when
-    var result = MoreGraphs.buildGraphWithBreadthFirstTraversal(ImmutableList.of(1), expectedGraph);
-
-    // then
-    assertThat(result).isEqualTo(expectedGraph);
-  }
-
-  @Test
-  void
-      whenBuildingGraphWithBftAndUndirectedGuavaGraph_thenResultHasTwoDirectedEdgesPerUndirectedEdgeInTheInputGraph() {
-
-    // given
-    var successorsFunction =
-        GraphBuilder.undirected().allowsSelfLoops(false).<Integer>immutable().putEdge(1, 2).build();
-
-    // when
-    ImmutableGraph<Integer> result =
-        MoreGraphs.buildGraphWithBreadthFirstTraversal(ImmutableList.of(1), successorsFunction);
-
-    // then
-    assertThat(result)
-        .isEqualTo(
-            GraphBuilder.directed()
-                .allowsSelfLoops(true)
-                .immutable()
-                .putEdge(1, 2)
-                .putEdge(2, 1)
-                .build());
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndNullSuccessorsFunction_thenNpeIsThrown() {
-    // when
-    ThrowingCallable codeUnderTest =
-        () -> MoreGraphs.buildGraphWithBreadthFirstTraversal(Set.of(), null);
-
-    // then
-    assertThatCode(codeUnderTest)
-        .as(
-            "MoreGraphs.buildGraphWithBreadthFirstTraversal(anyStartingNodes, null) "
-                + "expected to throw NullPointerException")
-        .isInstanceOf(NullPointerException.class)
-        .hasMessageContaining("successorsFunction");
-  }
-
-  @Test
-  void whenBuildingGraphWithBftAndNullStartingNodes_thenNpeIsThrown() {
-    // when
-    ThrowingCallable codeUnderTest =
-        () -> MoreGraphs.buildGraphWithBreadthFirstTraversal(null, element -> Set.of());
-
-    // then
-    assertThatCode(codeUnderTest)
-        .as(
-            "MoreGraphs.buildGraphWithBreadthFirstTraversal(null, anySuccessorsFunction) "
-                + "expected to throw NullPointerException")
-        .isInstanceOf(NullPointerException.class)
-        .hasMessageContaining("startingNodes");
   }
 
   @Test
@@ -552,7 +411,7 @@ class MoreGraphsTests {
 
   @Test
   void
-      givenTableAsValueGraph_whenGettingEdgeValueOrDefaultOfUnrelatedRowAndColumnKeys_thenIsDefaultEdgeValue() {
+  givenTableAsValueGraph_whenGettingEdgeValueOrDefaultOfUnrelatedRowAndColumnKeys_thenIsDefaultEdgeValue() {
     // given
     var table =
         ImmutableTable.builder() //
@@ -631,7 +490,7 @@ class MoreGraphsTests {
 
   @Test
   void
-      givenTableAsValueGraph_whenGettingEdgeValueOrDefaultWhereColumnKeyNotInTable_thenThrowsIae() {
+  givenTableAsValueGraph_whenGettingEdgeValueOrDefaultWhereColumnKeyNotInTable_thenThrowsIae() {
     // given
     var table = singleCellTable();
     var graph = MoreGraphs.asValueGraph(table);
@@ -670,7 +529,7 @@ class MoreGraphsTests {
 
   @Test
   void
-      givenTableAsValueGraph_whenGettingEdgeValueOrDefaultOfAbsentEndpointsEdge_thenIsDefaultEdgeValue() {
+  givenTableAsValueGraph_whenGettingEdgeValueOrDefaultOfAbsentEndpointsEdge_thenIsDefaultEdgeValue() {
     // given
     var table =
         ImmutableTable.builder() //
@@ -733,7 +592,7 @@ class MoreGraphsTests {
 
   @Test
   void
-      givenTableAsValueGraph_whenGettingEdgeValueOrDefaultWhereFirstEndpointNotInTable_thenThrowsIae() {
+  givenTableAsValueGraph_whenGettingEdgeValueOrDefaultWhereFirstEndpointNotInTable_thenThrowsIae() {
     // given
     var table = singleCellTable();
     var graph = MoreGraphs.asValueGraph(table);
@@ -755,7 +614,7 @@ class MoreGraphsTests {
 
   @Test
   void
-      givenTableAsValueGraph_whenGettingEdgeValueOrDefaultWhereLastEndpointNotInTable_thenThrowsIae() {
+  givenTableAsValueGraph_whenGettingEdgeValueOrDefaultWhereLastEndpointNotInTable_thenThrowsIae() {
     // given
     var table = singleCellTable();
     var graph = MoreGraphs.asValueGraph(table);

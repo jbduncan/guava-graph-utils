a5bbd0cee49787ec22d169c65f7097f0049b35b0
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2021-04-20 18:34:02 +0100

    Upgrade versions and improve build reproducibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     java
-    id("com.github.ben-manes.versions") version ("0.36.0")
-    id("com.diffplug.spotless") version ("5.9.0")
+    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.diffplug.spotless") version "5.12.1"
 }
 
 group = "org.jbduncan"
@@ -12,10 +14,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.guava:guava:30.1-jre")
+    implementation("com.google.guava:guava:30.1.1-jre")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    testImplementation("org.assertj:assertj-core:3.15.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testImplementation("org.assertj:assertj-core:3.19.0")
 }
 
 tasks.test {
@@ -28,8 +30,26 @@ java {
     }
 }
 
+tasks.withType<DependencyUpdatesTask>().configureEach {
+    fun isStable(version: String): Boolean {
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        return stableKeyword || regex.matches(version)
+    }
+
+    rejectVersionIf {
+        isStable(currentVersion) && !isStable(candidate.version)
+    }
+}
+
 spotless {
     java {
-        googleJavaFormat("1.9")
+        target("src/**/*.java")
+        googleJavaFormat("1.10.0")
     }
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
 }
